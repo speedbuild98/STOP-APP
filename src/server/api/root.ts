94879d52416d91b34be0813f82de1f4ad58f068f
speedbuild98 <lautaropectm98@gmail.com>
2023-06-16 18:32:00 -0300

    basic app working

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { exampleRouter } from "~/server/api/routers/example";
 import { createTRPCRouter } from "~/server/api/trpc";
+import { addictionRouter } from "~/server/api/routers/addiction";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  addiction: addictionRouter,
 });
 
 // export type definition of API
